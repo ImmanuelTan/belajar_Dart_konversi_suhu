9049ee08fb9514d1b507936761ed41ffb77d07cc
ImmanuelTan <immanuel1828@gmail.com>
2022-06-01 20:01:15 +0700

    TAMBAH CATCH

--- bin/konversi_suhu.dart
+++ bin/konversi_suhu.dart
@@ -13,31 +13,35 @@ void main(List<String> arguments) {
   stdout.write("masukkan suhu : ");
   var suhu = num.parse(stdin.readLineSync()!);
 
-  if( pil == 1){
-    cel = suhu;
-    fah = suhu * (9/5) + 32;
-    re  = suhu * (4/5);
-    kel = suhu + 273;
-  }else if( pil == 2){
-    fah = suhu;
-    cel = (suhu - 32) * 5/9;
-    re  = (suhu - 32) * 4/9;
-    kel = (suhu - 32) * 5/9 + 273;
-  }else if( pil == 3) {
-    re = suhu;
-    cel = suhu * (5 / 4);
-    fah = suhu * (9 / 4) + 32;
-    kel = suhu * (5 / 4) + 273;
-  }else if( pil == 4){
-    kel = suhu;
-    cel = suhu - 273;
-    fah = (suhu - 273) *  9 / 4 + 32;
-    re  = (suhu - 273) * 4 / 5;
+  try {
+    if (pil == 1) {
+      cel = suhu;
+      fah = suhu * (9 / 5) + 32;
+      re = suhu * (4 / 5);
+      kel = suhu + 273;
+    } else if (pil == 2) {
+      fah = suhu;
+      cel = (suhu - 32) * 5 / 9;
+      re = (suhu - 32) * 4 / 9;
+      kel = (suhu - 32) * 5 / 9 + 273;
+    } else if (pil == 3) {
+      re = suhu;
+      cel = suhu * (5 / 4);
+      fah = suhu * (9 / 4) + 32;
+      kel = suhu * (5 / 4) + 273;
+    } else if (pil == 4) {
+      kel = suhu;
+      cel = suhu - 273;
+      fah = (suhu - 273) * 9 / 4 + 32;
+      re = (suhu - 273) * 4 / 5;
+    }
+    print("Hasil dari konversi suhu \n"
+        "Suhu Celcius     = $cel \n"
+        "Suhu Fahrenheit  = $fah\n"
+        "Suhu Reamur      = $re\n"
+        "Suhu Kelvin      = $kel");
   }
-  print("Hasil dari konversi suhu \n"
-      "Suhu Celcius     = $cel \n"
-      "Suhu Fahrenheit  = $fah\n"
-      "Suhu Reamur      = $re\n"
-      "Suhu Kelvin      = $kel");
-
+  catch (e) {
+    print('Exception happened: $e');
+  }
 }
